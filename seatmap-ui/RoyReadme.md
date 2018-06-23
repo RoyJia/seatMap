5e79484c39062db3559041b352d6f21172119d9d
RoyJia <v-rjia@expedia.com>
2018-06-24 01:53:20 +0800

    adding dragging zooming

---
+++ seatmap-ui/RoyReadme.md
@@ -0,0 +1,15 @@
+
+## 原生JavaScript实现拖拽
+http://huang-jerryc.com/2014/07/12/%E5%8E%9F%E7%94%9FJavaScript%E5%AE%9E%E7%8E%B0%E6%8B%96%E6%8B%BD%E6%95%88%E6%9E%9C/
+
+## SVG画布、坐标系统、视窗
+https://www.w3cplus.com/svg/svg-viewport.html
+
+## 理解SVG坐标系和变换：视窗,viewBox和preserveAspectRatio
+http://www.w3cplus.com/html5/svg-coordinate-systems.html
+
+## Styling & Animating SVG with css
+https://docs.google.com/presentation/d/1Iuvf3saPCJepVJBDNNDSmSsA0_rwtRYehSmmSSLYFVQ/pub?start=false&loop=false&delayms=3000&slide=id.p
+
+## Clicking and Dragging SVG with React and D3js
+https://medium.com/@mikesurowiec/clicking-and-dragging-svg-with-react-and-d3-js-5639cd0c3c3b
--- seatmap-ui/src/containers/FullMap.js
+++ seatmap-ui/src/containers/FullMap.js
@@ -2,23 +2,48 @@ import React from 'react';
 import Seat from '../components/Seat';
 import SearchBox from '../components/searchBox/SearchBox';
 import data from '../data.json';
+import * as d3 from 'd3';
+import Dimensions from 'react-dimensions';
 
 class FullMap extends React.Component {
 
+  componentDidMount() {
+    const contentWrap = d3.select('#contentWrap');
+    const svg = d3.select('svg');
+    // contentWrap.attr('pointer-events', 'none');
+
+    const zoomed = () => {
+      const {k, x, y} = d3.event.transform;
+      contentWrap.attr('transform', `translate(${x}, ${y}) scale(${k})`);
+    }
+
+    const zoom = d3.zoom()
+      .scaleExtent([1, 5])
+      // .translateExtent([[-100, -100], [this.state.svgViewportWidth-500, this.state.svgViewportHeight-500]])
+      .on('zoom', zoomed);
+
+    svg.append('rect')
+      .attr('class', 'mouse-capture')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', '100vw')
+      .attr('height', '100vh')
+      .style('fill', 'white')
+      .lower() // put it below the map
+      .call(zoom);
+  }
 
   render () {
     return (
       <div>
         <SearchBox/>
 
-        <svg width="1680" height="1050" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            <rect x="-1" y="-1" width="1680" height="1050" id="canvas_background" fill="#fff"/>
-            <g id="canvasGrid" display="none">
-              <rect id="svg_1" width="100%" height="100%" x="0" y="0" strokeWidth="0" fill="url(#gridpattern)"/>
-            </g>
-          </g>
-          <g>
+        <svg
+          width="100vw" height="100vh"
+          // viewBox={[0, 0, this.state.svgViewportWidth, this.state.svgViewportHeight].join(' ')}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="contentWrap">
             {
               data.map((item) => {
                 return <Seat key={item.seat.id} {...item.seat}/>;
@@ -45,4 +70,4 @@ class FullMap extends React.Component {
   }
 }
 
-export default FullMap;
+export default Dimensions()(FullMap);
